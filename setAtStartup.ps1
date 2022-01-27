edf2b6b865439da50614aaed042d4698afd19fce
BenoitEirik <olafsergent@gmail.com>
2022-01-27 14:25:50 +0100

    First commit

---
+++ setAtStartup.ps1
@@ -0,0 +1,3 @@
+﻿$action = New-ScheduledTaskAction -Execute "powershell" -Argument "-ExecutionPolicy ByPass -File C:\WOTD\wotd.ps1"
+$trigger = New-ScheduledTaskTrigger -AtLogon
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "WallpaperOfTheDay" -Description "Daily change wallpaper"
---
+++ wotd.ps1
@@ -0,0 +1,25 @@
+﻿$WebClient = New-Object System.Net.WebClient
+$json = $WebClient.DownloadString("https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=fr-FR")
+
+$url = $json | ConvertFrom-Json
+$WebClient.DownloadFile("https://bing.com" + $url.images.url,"C:\WOTD\wotd.jpg")
+
+$setwallpapersrc = @"
+using System.Runtime.InteropServices;
+
+public class Wallpaper
+{
+  public const int SetDesktopWallpaper = 20;
+  public const int UpdateIniFile = 0x01;
+  public const int SendWinIniChange = 0x02;
+  [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+  private static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+  public static void SetWallpaper(string path)
+  {
+    SystemParametersInfo(SetDesktopWallpaper, 0, path, UpdateIniFile | SendWinIniChange);
+  }
+}
+"@
+Add-Type -TypeDefinition $setwallpapersrc
+
+[Wallpaper]::SetWallpaper("C:\WOTD\wotd.jpg")
