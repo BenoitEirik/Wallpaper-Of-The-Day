60e1a744b946168ee992fcac21dfc5450a647de7
BenoitEirik <olafsergent@gmail.com>
2022-01-30 13:17:50 +0100

    Hide window at startup

--- setAtStartup.ps1
+++ setAtStartup.ps1
@@ -1,3 +1,3 @@
-﻿$action = New-ScheduledTaskAction -Execute "powershell" -Argument "-ExecutionPolicy ByPass -File C:\WOTD\wotd.ps1"
+﻿$action = New-ScheduledTaskAction -Execute "powershell" -Argument "-WindowStyle Hidden -ExecutionPolicy ByPass -File C:\WOTD\wotd.ps1"
 $trigger = New-ScheduledTaskTrigger -AtLogon
 Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "WallpaperOfTheDay" -Description "Daily change wallpaper"
