8c0472fcb1f8aa33138d1f60a9f8bcd15c6dd71c
BenoitEirik <olafsergent@gmail.com>
2022-02-02 09:46:08 +0100

    Switch cscript to wscript to hide VBS window

--- wotd.ps1
+++ wotd.ps1
@@ -7,7 +7,7 @@ if ($inst -eq "install") {
     $localeCode = "fr-FR"
   }
 
-  $action = New-ScheduledTaskAction -Execute "cscript" -Argument "//nologo C:\WOTD\startup.vbs $($localeCode)"
+  $action = New-ScheduledTaskAction -Execute "wscript" -Argument "//nologo C:\WOTD\startup.vbs $($localeCode)"
   $trigger = New-ScheduledTaskTrigger -AtLogon
   Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "WallpaperOfTheDay" -Description "Daily change wallpaper"
   Exit
